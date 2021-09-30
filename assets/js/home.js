1850b400b43427dbf6b2bac85bdfc4f332252376
Hoang Huyen <1851061774@e.tlu.edu.vn>
2021-09-30 17:55:48 +0700

    update 123

--- assets/js/home.js
+++ assets/js/home.js
@@ -22,3 +22,87 @@ const viewer = new PANOLENS.Viewer({
 
 viewer.add(panorama);
 
+//xử lý nút click menu
+$(document).ready(function(){
+    $('.menu').show();
+    // $('.click-menu').hide();
+    $('.icon').click(function(event){
+        $('.menu').toggle();
+        // $('.click-menu-hidden').hide();
+        // $('.click-menu').show();
+        // $('.click-menu').css("margin-top","10px")
+    });
+});
+
+//xử lý khi click vào close
+$(document).ready(function(){
+    $('.icon-close').click(function(event){
+        $('.form-huongdan').hide();
+        document.getElementById("anh360").style.opacity = '1';
+    });
+    $('.video').click(function(event){
+        $('.form-huongdan').hide();
+        document.getElementById("anh360").style.opacity = '1';
+    });
+    $('.menu').click(function(event){
+        $('.form-huongdan').hide();
+        document.getElementById("anh360").style.opacity = '1';
+    });
+});
+
+//slick
+$('.center').slick({
+   
+    centerMode: true,
+    centerPadding: '30px',
+    slidesToShow: 6,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+  /* xử lý show owl */
+  $(document).ready(function(){
+      $('.fa-th-large').click(function(event){
+          $('.owl').toggle();
+        
+      });
+  });
+
+  /* xử lý khi click vào volum */
+  
+  $(document).ready(function(){
+    $('.fa-volume-up').click(function(event){
+        const id= this.id.split('_')[1];
+        var checkid = $(this).attr('class').split(' ');
+        if(checkid.find(e=>e=='active')=='active'){
+            $(this).removeClass('active');
+            $('.fa-volume-up').hide();
+            $('.fa-volume-mute').show();
+        }
+        else{
+            $(this).addClass('active');
+            $('.fa-volume-up').show();
+        }
+        
+
+       
+    });
+});
